a9386d1c72836a80dff01e2b91dbb3e73ec86e2d
Daniel Alves Pereira <rm87382@estudante.fieb.edu.br>
2023-08-28 20:29:11 -0300

    criação do ambiente de desenvolvimento

---
+++ src/test/java/com/itb/tcc/cinepoc/CinepocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.cinepoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinepocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
